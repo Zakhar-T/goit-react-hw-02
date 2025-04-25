40c8db26a8aa8da6b326f3ddea35fc7ff5e6614b
Zakhar-T <zakhar.null@gmail.com>
2025-04-25 22:42:51 +0300

    add file structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,5 @@
-import userData from '../Profile/userData.json';
-import friends from '../FriendList/friends.json';
-import transactions from '../TransactionHistory/transactions.json';
-import Profile from '../Profile/Profile';
-import FriendList from '../FriendList/FriendList';
-import TransactionHistory from '../TransactionHistory/TransactionHistory';
 import css from './App.module.css';
 
 export default function App() {
-  return (
-    <div className={css.container}>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </div>
-  );
+  return <div className={css.container}></div>;
 }
